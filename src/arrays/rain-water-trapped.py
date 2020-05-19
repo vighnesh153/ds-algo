314780228e3a694f2640b0435c1e8fa4978559bb
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-19 11:31:56 +0530

    optimize on variables, lines of code and better naming

--- src/arrays/rain-water-trapped.py
+++ src/arrays/rain-water-trapped.py
@@ -1,26 +1,20 @@
 def solve(arr):
-    left_max = [0] * len(arr)
-    right_max = [0] * len(arr)
-
-    curr_max = -1
+    prefix_max = [0] * len(arr)
     for i in range(1, len(arr)):
-        curr_max = max(curr_max, arr[i - 1])
-        left_max[i] = curr_max
+        prefix_max[i] = max(prefix_max[i - 1], arr[i - 1])
 
-    curr_max = -1
-    for i in range(len(arr) - 2, -1, -1):
-        curr_max = max(curr_max, arr[i + 1])
-        right_max[i] = curr_max
+    suffix_max = [0] * len(arr)
+    for j in range(len(arr) - 2, -1, -1):
+        suffix_max[j] = max(suffix_max[j + 1], arr[j + 1])
 
-    total = 0
-    for i in range(1, len(arr) - 1):
-        height = min(left_max[i], right_max[i])
-        water_trapped = height - arr[i]
-        if water_trapped >= 0:
-            total += water_trapped
+    area = 0
+    for i, elem in enumerate(arr):
+        current_area = min(prefix_max[i], suffix_max[i]) - elem
+        if current_area > 0:
+            area += current_area
 
-    return total
+    return area
 
 
-res = solve([1, 2])
+res = solve([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
 print(res)
